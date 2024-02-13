74ef58c8f9092b8754a32ffdb91dc5824ed16409
crgimenes <crg@crg.eti.br>
2024-02-12 21:52:02 -0300

    update packages

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.22.0
 require (
 	github.com/PullRequestInc/go-gpt3 v1.2.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/go-telegram/bot v1.0.1
+	github.com/sashabaranov/go-openai v1.19.3
 )
--- main.go
+++ main.go
@@ -2,182 +2,151 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"strings"
+	"os/signal"
 
-	"github.com/PullRequestInc/go-gpt3"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 var (
-	maxTokens = 3000
+	openaiAPIKey     string
+	telegramBotToken string
+	systemContext    []byte
 )
 
-func createPrompt(magacc []string, logMsg string) string {
-	msgContext := ""
-	for _, v := range magacc {
-		msgContext += v
+func getOpenAI(userQuery []string) (string, error) {
+	c := openai.NewClient(openaiAPIKey)
+	ctx := context.Background()
+
+	context := ""
+	for _, v := range userQuery[:len(userQuery)-1] {
+		context += v + "\n\n"
 	}
 
-	preContext, err := os.ReadFile("pre_ctx.txt")
-	if err != nil {
-		if !os.IsNotExist(err) {
-			log.Println(err)
-		}
+	systemQuery := openai.ChatCompletionMessage{
+		Role: openai.ChatMessageRoleSystem,
+		Content: fmt.Sprintf("%s\nmensagens de contexto:%s ",
+			systemContext, // pre-existing context
+			context),      // user context
 	}
 
-	posContext, err := os.ReadFile("pos_ctx.txt")
-	if err != nil {
-		if !os.IsNotExist(err) {
-			log.Println(err)
-		}
+	message := []openai.ChatCompletionMessage{
+		systemQuery,
 	}
 
-	prompt := string(preContext) + msgContext + "\n---\n" + string(posContext) + logMsg
-	return prompt
-}
-
-func loadContext() []string {
-	magacc := []string{}
-
-	b, err := os.ReadFile("ctx.json")
-	if err != nil {
-		if !os.IsNotExist(err) {
-			log.Fatalln(err)
-		}
-	}
-	if len(b) > 0 {
-		err = json.Unmarshal(b, &magacc)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-	return magacc
-}
-
-func saveContext(magacc []string) {
-	b, err := json.Marshal(magacc)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = os.WriteFile("ctx.json", b, 0644)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func sendToTelegram(update tgbotapi.Update, response string, bot *tgbotapi.BotAPI) {
-	msg := tgbotapi.Chattable(&tgbotapi.MessageConfig{
-		BaseChat: tgbotapi.BaseChat{
-			ChatID:           update.Message.Chat.ID,
-			ReplyToMessageID: update.Message.MessageID,
-		},
-		Text:                  response,
-		ParseMode:             "markdown",
-		DisableWebPagePreview: true,
+	message = append(message, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: userQuery[len(userQuery)-1], // last message is the user query
 	})
 
-	bot.Send(msg)
+	req := openai.ChatCompletionRequest{
+		Model:     openai.GPT3Dot5Turbo,
+		MaxTokens: 2000,
+		Messages:  message,
+		Stream:    true,
+	}
+	stream, err := c.CreateChatCompletionStream(ctx, req)
+	if err != nil {
+		return "", err
+	}
+	defer stream.Close()
+
+	r := ""
+	for {
+		response, err := stream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return r, nil
+			}
+			return "", err
+		}
+		r += response.Choices[0].Delta.Content
+	}
 }
 
 func main() {
-	chatMsgs := loadContext()
-
-	apiKey := os.Getenv("OPENAI_API_KEY")
-	if apiKey == "" {
-		log.Fatalln("Missing OPENAI_API_KEY")
+	var err error
+	openaiAPIKey = os.Getenv("OPENAI_API_KEY")
+	if openaiAPIKey == "" {
+		fmt.Println("OPENAI_API_KEY environment variable is not set")
+		return
 	}
 
-	telegramBotToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	telegramBotToken = os.Getenv("TELEGRAM_BOT_TOKEN")
 	if telegramBotToken == "" {
-		log.Fatalln("Missing TELEGRAM_BOT_TOKEN")
+		fmt.Println("TELEGRAM_BOT_TOKEN environment variable is not set")
+		return
 	}
 
-	client := gpt3.NewClient(apiKey)
-	ctx := context.Background()
-
-	bot, err := tgbotapi.NewBotAPI(telegramBotToken)
+	systemContext, err = os.ReadFile("ctx.txt")
 	if err != nil {
-		log.Panic(err)
+		if !os.IsNotExist(err) {
+			log.Fatalf("Error reading pre_ctx.txt: %v", err)
+		}
+		log.Println("ctx.txt not found")
 	}
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	/*
+		userQuery := []string{}
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		logMsg := fmt.Sprintf("\n---\nFrom: %q\nMessage: %s\n", update.Message.From.UserName, update.Message.Text)
-
-		fmt.Printf("Received message: %s\n", update.Message.Text)
-
-		if len(chatMsgs) > 5 {
-			chatMsgs = chatMsgs[len(chatMsgs)-5:]
-		}
-
-		//if update.Message.Chat.UserName != bot.Self.UserName &&
-		//	!strings.Contains(update.Message.Text, bot.Self.UserName) {
-		//	continue
-		//}
-
-		buf := strings.Builder{}
-
-		prompt := createPrompt(chatMsgs, logMsg)
-		maxRetries := 3 // TODO: make this configurable
-		retries := maxRetries
-	retry:
-		buf.Reset()
-		err = client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
-			Prompt: []string{
-				prompt,
-			},
-			MaxTokens:   gpt3.IntPtr(maxTokens),
-			Temperature: gpt3.Float32Ptr(0.5), // TODO: make this configurable
-		}, func(resp *gpt3.CompletionResponse) {
-			buf.WriteString(resp.Choices[0].Text)
-		})
-		if err != nil {
-			log.Printf("GPT-3 error: %s, retrying n: %d", err, maxRetries-retries+1)
-			if len(chatMsgs) > 1 {
-				chatMsgs = chatMsgs[:len(chatMsgs)-1]
-			}
-			retries--
-			if retries <= 0 {
-				log.Printf("GPT-3 error: %s, max retries reached", err)
-				// clear
-				chatMsgs = []string{}
-				buf.Reset()
+		for update := range updates {
+			if update.Message == nil { // ignore any non-Message Updates
 				continue
 			}
-			goto retry // goto is not evil
+
+			logMsg := fmt.Sprintf("\n---\nFrom: %q\nMessage: %s\n", update.Message.From.UserName, update.Message.Text)
+
+			userQuery = append(userQuery, logMsg)
+			fmt.Printf("Received message: %s\n", update.Message.Text)
 		}
 
-		response := buf.String()
+		msg := []string{"Qual sua opinião sobre rust?"}
 
-		if strings.Contains(response, "++++") {
-			log.Printf("log: %q msg ignored: %q\n", logMsg, response)
-			continue
+		r, err := getOpenAI(msg)
+		if err != nil {
+			log.Fatalf("Error getting OpenAI response: %v", err)
 		}
+		fmt.Println(r)
+	*/
 
-		if response == "" {
-			log.Println("empty response")
-			continue
-		}
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-		log.Printf("msg: %s", response)
-
-		chatMsgs = append(chatMsgs, logMsg, response)
-		saveContext(chatMsgs)
-		sendToTelegram(update, response, bot)
+	opts := []bot.Option{
+		bot.WithDefaultHandler(handler),
 	}
+
+	b, err := bot.New(telegramBotToken, opts...)
+	if err != nil {
+		log.Fatalf("Error creating bot: %v", err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	if update.Message == nil {
+		return
+	}
+
+	from := update.Message.From
+	logMsg := fmt.Sprintf("\n---\nFrom: %q\nMessage: %s\n", from.Username, update.Message.Text)
+
+	r, err := getOpenAI([]string{string(logMsg)})
+	if err != nil {
+		log.Printf("Error getting OpenAI response: %v", err)
+		return
+	}
+
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ParseMode: models.ParseModeMarkdown,
+		ChatID:    update.Message.Chat.ID,
+		Text:      r,
+	})
 }
